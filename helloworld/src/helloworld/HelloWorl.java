ec3dae608dcdab74da6a790c52cc465533993d67
Carlos Moroni Garcia <carlosmoronisud@gmail.com>
2025-04-11 11:15:43 -0300

    :tada: Commit Inicial

---
+++ helloworld/src/helloworld/HelloWorl.java
@@ -0,0 +1,12 @@
+package helloworld;
+
+public class HelloWorl {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Worl");
+		
+		System.out.println("Python é melhor que java");
+	}
+
+}
